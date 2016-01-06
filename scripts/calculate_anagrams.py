45c9bb4ff514fa6256ab4c13d704996bb997a164
Albert Sheu <albert.sheu@gmail.com>
2016-01-05 23:40:00 -0800

    Modularize calculate_anagrams.py

--- scripts/calculate_anagrams.py
+++ scripts/calculate_anagrams.py
@@ -3,34 +3,38 @@ import itertools
 import argparse
 import json
 
-parser = argparse.ArgumentParser(description='Create an anagram build for Text Twist')
-parser.add_argument(
-  'words',
-  metavar='WORD',
-  type=str,
-  nargs='+',
-  help='A string to anagramize'
-)
-parser.add_argument(
-    '--dict',
-    dest='dictfile',
-    default='/usr/share/dict/words',
-    help='Dictionary file'
-)
-args = parser.parse_args()
 
-words = set(open(args.dictfile).read().split())
+def anagrams(word, dictionary):
+    output = []
+    for wlen in range(3, len(word)+1):
+        for possible in itertools.permutations(word, wlen):
+            possible_word = ''.join(possible)
+            if possible_word in dictionary:
+                output.append(possible_word.upper())
+    obj = {
+        'word': word.upper(),
+        'anagrams': sorted(set(output))
+    }
+    return json.dumps(obj, indent=4, separators=(', ', ': '))
+
 
 if __name__ == '__main__':
-    for to_split in args.words:
-        output = []
-        for wlen in range(3, len(to_split)+1):
-            for possible in itertools.permutations(to_split, wlen):
-                possible_word = ''.join(possible)
-                if possible_word in words:
-                    output.append(possible_word.upper())
-        obj = {
-            'word': to_split.upper(),
-            'anagrams': sorted(set(output))
-        }
-        print(json.dumps(obj, indent=4, separators=(', ', ': ')))
+    parser = argparse.ArgumentParser(description='Create an anagram build for Text Twist')
+    parser.add_argument(
+      'words',
+      metavar='WORD',
+      type=str,
+      nargs='+',
+      help='A string to anagramize'
+    )
+    parser.add_argument(
+        '--dict',
+        dest='dictfile',
+        default='/usr/share/dict/words',
+        help='Dictionary file'
+    )
+    args = parser.parse_args()
+
+    dictionary = set(open(args.dictfile).read().split())
+    for word in args.words:
+        print(anagrams(word, dictionary))
